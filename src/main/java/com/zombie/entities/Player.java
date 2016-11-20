5a2e2635e005262c3ca4b2c23760e58d19d6a3b0
Amrinder Arora <aarora@bizmerlin.com>
2016-11-20 09:03:23 -0500

    Added persistence dependency in pom.

--- src/main/java/com/zombie/entities/Player.java
+++ src/main/java/com/zombie/entities/Player.java
@@ -7,61 +7,74 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="zombie.player")
+@Table(name = "zombie.player")
 public class Player {
 
-     @Id
-     @GeneratedValue(strategy = GenerationType.IDENTITY)
-     
-	 private int playerid;
-	 public String name = null;
-	 String species = null;
-	 int points = 0;
-	 double locationx = 0;
-	 double locationy = 0;
-	 long createts = 0;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int playerid;
 
-public int get_Id(){
-     return playerid;
-}
-public void set_Id(int id){
-this.playerid = id;
-}
-public String getName(){
-return name;
-}
-public void setName(String name){
-this.name = name;
-}
-public String getSpecies(){
-return species;
-}
-public void setSpecies(String species){
-this.species = species;
-}
-public int getPoints(){     
-return points;
-}
-public void setPoints(int points){
-     this.points = points;
-}
-public double getLocationx(){     
-return locationx;
-}
-public void setLocationx(double locationx){
-     this.locationx = locationx;
-}
-public double getLocationy(){     
-return locationy;
-}
-public void setLocationy(double locationy){
-     this.locationy = locationy;
-}
-public long getCreatets(){
-     return createts;
-}
-public void setCreatets(long createts){
-     this.createts = createts;
-}
-     
+	public String name = null;
+	String species = null;
+	int points = 0;
+	double locationx = 0;
+	double locationy = 0;
+	long createts = 0;
+
+	public int get_Id() {
+		return playerid;
+	}
+
+	public void set_Id(int id) {
+		this.playerid = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSpecies() {
+		return species;
+	}
+
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
+	public double getLocationx() {
+		return locationx;
+	}
+
+	public void setLocationx(double locationx) {
+		this.locationx = locationx;
+	}
+
+	public double getLocationy() {
+		return locationy;
+	}
+
+	public void setLocationy(double locationy) {
+		this.locationy = locationy;
+	}
+
+	public long getCreatets() {
+		return createts;
+	}
+
+	public void setCreatets(long createts) {
+		this.createts = createts;
+	}
+
 }
