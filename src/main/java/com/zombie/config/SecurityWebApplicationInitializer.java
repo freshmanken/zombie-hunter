de666c1cd3f08e3512c1ccb81b8966a789a7c31c
freshmanken <luanjian0817@gmail.com>
2017-01-07 12:19:12 -0500

    Spring Security java configuration

---
+++ src/main/java/com/zombie/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.zombie.config;
+
+import org.springframework.security.web.context.*;
+
+public class SecurityWebApplicationInitializer
+	extends AbstractSecurityWebApplicationInitializer {
+}
+
