0d3337c01d9770025a58c7d5d871cd18fcf683e5
freshmanken <luanjian0817@gmail.com>
2017-02-01 12:42:45 -0500

    db information moved from tomcat context.xml to rootconfig.java

--- src/main/java/com/zombie/config/RootConfig.java
+++ src/main/java/com/zombie/config/RootConfig.java
@@ -2,10 +2,10 @@ package com.zombie.config;
 
 import javax.sql.DataSource;
 
+import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
@@ -15,9 +15,21 @@ public class RootConfig {
 	
 	@Bean
 	public DataSource dataSource() {
-		final JndiDataSourceLookup jndiDataSourceLookup = new JndiDataSourceLookup();
-		jndiDataSourceLookup.setResourceRef(true);
-		DataSource dataSource = jndiDataSourceLookup.getDataSource("jdbc/ZombieDS");
-		return dataSource;
+		/* this is used for datasource defined in tomcat context.xml */
+		// final JndiDataSourceLookup jndiDataSourceLookup = new
+		// JndiDataSourceLookup();
+		// jndiDataSourceLookup.setResourceRef(true);
+		// DataSource dataSource =
+		// jndiDataSourceLookup.getDataSource("jdbc/ZombieDS");
+		// return dataSource;
+
+		BasicDataSource basicDataSource = new org.apache.commons.dbcp.BasicDataSource();
+		basicDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		basicDataSource.setUrl(
+				"jdbc:mysql://zrds2.chectpppt5lt.us-west-2.rds.amazonaws.com:3306/zmb?zeroDateTimeBehavior=convertToNull");
+		basicDataSource.setUsername("zdev");
+		basicDataSource.setPassword("hk=fir6l?4pc");
+		return basicDataSource;
+
 	}
 }
